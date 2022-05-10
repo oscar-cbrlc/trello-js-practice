05e2152e018c8599040a7b7b131aec175a53d099
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-05-10 18:31:55 -0500

    Added addCard method

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 require('dotenv').config()
 
-if (!process.env.TOKEN && !process.env.KEY){
-    throw new Error('No hay configuración con Api Key y con Token')
+if(!process.env.TOKEN && !process.env.KEY){
+  throw new Error('No hay configuración con Api Key y con Token')
 }
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let cardTitle = `Card Nueva ${new Date()}`
+
+trello.addCard(cardTitle, "LaunchX Card Description", "62747dfc902fbe49c76322c0",
+	function (error, trelloCard) {
+		if (error) {
+			console.log('Could not add card:', error);
+		}
+		else {
+			console.log('Added card:', trelloCard);
+		}
+	});
